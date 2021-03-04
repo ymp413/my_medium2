1e2ac6d48ce30cffdd65ff641e93c1aa005d8bf9
ymp413 <ymp413@gmail.com>
2021-03-05 03:06:12 +0800

    1.7 編輯及刪除故事

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,8 +1,29 @@
 class StoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_story, only: [:edit, :update, :destroy]
+
+  def index
+    @stories = current_user.stories.order(created_at: :desc)
+  end
 
   def new
     @story = current_user.stories.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to stories_path, notice: '故事更新成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @story.destroy
+    redirect_to stories_path, notice: '故事已刪除'    
   end
 
   def create
@@ -16,6 +37,9 @@ class StoriesController < ApplicationController
   end
 
   private
+  def find_story
+    @story = current_user.stories.find(params[:id])
+  end
   def story_params
     params.require(:story).permit(:title, :content)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_193541) do
+ActiveRecord::Schema.define(version: 2021_03_04_183504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_03_03_193541) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_stories_on_deleted_at"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
