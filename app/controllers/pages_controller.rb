5f98be285f4a5ef501c2ec909cd63994334c1b8b
ymp413 <ymp413@gmail.com>
2021-03-06 04:41:22 +0800

    1.20 故事內容頁

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :find_story, only: [:show]
   
   def index
     @stories = Story.published_stories
@@ -9,5 +10,10 @@ class PagesController < ApplicationController
 
   def user
     
+  end
+
+  private
+  def find_story
+    @story = Story.friendly.find(params[:story_id])
   end
 end
