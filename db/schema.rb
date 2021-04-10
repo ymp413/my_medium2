23c663fb7929b00891646dc793d503ec1a9c3fe6
ymp413 <ymp413@gmail.com>
2021-04-11 05:26:23 +0800

    1.24 拍手功能 part1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_164705) do
+ActiveRecord::Schema.define(version: 2021_04_08_062954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_164705) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
     t.string "slug"
+    t.integer "clap", default: 0
     t.index ["deleted_at"], name: "index_stories_on_deleted_at"
     t.index ["slug"], name: "index_stories_on_slug", unique: true
     t.index ["user_id"], name: "index_stories_on_user_id"
