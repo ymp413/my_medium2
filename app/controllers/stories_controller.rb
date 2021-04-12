8afa350ced9791da9d10ab2269972de1e7afc26e
ymp413 <ymp413@gmail.com>
2021-04-13 02:33:30 +0800

    1.25 拍手功能 part2

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,13 +1,23 @@
 class StoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:clap]
   before_action :find_story, only: [:edit, :update, :destroy]
-
+  skip_before_action :verify_authenticity_token, only: [:clap]
   def index
     @stories = current_user.stories.order(created_at: :desc)
   end
 
   def new
     @story = current_user.stories.new
+  end
+
+  def clap
+    if user_signed_in?
+      story = Story.friendly.find(params[:id])
+      story.increment!(:clap)
+      render json: {status: story.clap}
+    else
+      render json: {status: 'sign_in_first'}
+    end
   end
 
   def edit
