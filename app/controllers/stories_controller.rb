93327c77be9999eeec5c4154979f296a26155448
ymp413 <ymp413@gmail.com>
2021-04-17 05:47:18 +0800

    1.26 Follow功能 part1

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,6 +2,7 @@ class StoriesController < ApplicationController
   before_action :authenticate_user!, except: [:clap]
   before_action :find_story, only: [:edit, :update, :destroy]
   skip_before_action :verify_authenticity_token, only: [:clap]
+  
   def index
     @stories = current_user.stories.order(created_at: :desc)
   end
